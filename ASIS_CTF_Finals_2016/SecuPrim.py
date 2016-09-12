356db70aef125cd41d49137f7391ebb2e6c77ef8
jason3e7 <jason3e7@gmail.com>
2016-09-12 12:23:10 +0800

    Fix SecuPrim.py problem solved

--- ASIS_CTF_Finals_2016/SecuPrim.py
+++ ASIS_CTF_Finals_2016/SecuPrim.py
@@ -1,6 +1,6 @@
-import sys, socket, hashlib, re
+import sys, socket, hashlib, re, gmpy2
 
-def findStr (input1, input2) :
+def findStr (input1, input2):
 	strSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	for s1 in range(0, len(strSet)):
 		for s2 in range(0, len(strSet)):
@@ -15,12 +15,30 @@ def findStr (input1, input2) :
 						#print hashString
 						return temp
 
-#def findStr (input1, input2) :
+def count (input1, input2):
+	print("Range size = " + str(input1 - input2))
+	count = 0
+	for i in range(input2, input1 + 1):
+		if gmpy2.is_prime(i):
+			count += 1
+		elif gmpy2.is_power(i):
+			count += 1
+	print("Count = " + str(count))
+	return count	
+
+def getSocketData(s, tails):
+	data = ""
+	while True:
+		for tail in tails:
+			if tail in data:
+				return data
+		data += s.recv(1)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('secuprim.asis-ctf.ir', 42738))
-print s.recv(1024)
-inputString = s.recv(1024)
+#print s.recv(1024)
+inputString = getSocketData(s, ["Enter X:"])
+#inputString = s.recv(1024)
 print inputString
 #pattern = "SHA256\(X + \"(*)\"\).hexdigest\(\) = \"(*)...\","
 pattern = r'SHA256\(X \+ \"(.+)\"\)\.hexdigest\(\) = \"(.+)\.\.\.\",'
@@ -30,24 +48,28 @@ reSet = re.search(pattern ,inputString)
 hashResult = findStr(reSet.group(1), reSet.group(2))
 print hashResult
 s.send(hashResult + '\n')
-print s.recv(1024)
-#print s.recv(1024)
-numberString = s.recv(5000)
-#print s.recv(1024)
-print numberString
-#numberString = "What's the number of primes or perfect powers like n such that: 5243269291454887441469100664450379632166513715971446363090767797743156123546121519300580839849071258764736630767760378406484795094273101304997073596485627357105933492198431259237777507988709432110022263714645521682212217307411628628958076902922410631563384500367776412842903161451250989637903182181518743726002740517362559994092779140605243592815811881190178313218604189184143277334720117905878912731423503923298943557821465938558211625137607975639497374125064389388228823883380819977773588273288320212036835950957795597201626150674157512513153850062638919746194221256104320064512157545263476047025186270716225470598690735830207192770289501288 <= n <= 5243269291454887441469100664450379632166513715971446363090767797743156123546121519300580839849071258764736630767760378406484795094273101304997073596485627357105933492198431259237777507988709432110022263714645521682212217307411628628958076902922410631563384500367776412842903161451250989637903182181518743726002740517362559994092779140605243592815811881190178313218604189184143277334720117905878912731423503923298943557821465938558211625137607975639497374125064389388228823883380819977773588273288320212036835950957795597201626150674157512513153850062638919746194221256104320064512157545263476047025186270716225470598690735830207192770289502756"
-pattern = r'What\'s the number of primes or perfect powers like n such that: (.+) <= n <= (.+)'
-reSet = re.search(pattern ,numberString)
-#print reSet.group(1)
-#print reSet.group(2)
-numberB = reSet.group(2)
-numberS = reSet.group(1)
-print int(numberB[-6:]) - int(numberS[-6:])
+data = getSocketData(s, "---\n")
+print data
 
-s.send('1111111' + '\n')
-print s.recv(1024)
-s.send('1111111' + '\n')
-print s.recv(1024)
+while True :
+	data = getSocketData(s, ["like n such", "corret!", "}"])
+	print data
 
-#https://www.wikiwand.com/en/Alphanumeric
-#PPC (Professional Programming and Coding)
+	if "ASIS" in data:
+		print data
+	
+	if "corret" in data:
+		print "failed"
+		break
+	else:
+		sentence = getSocketData(s, ["that: "])
+		sentence += getSocketData(s, ["\n"])
+		print sentence
+		b, e = re.findall("that: (\d+) <= n <= (\d+)", sentence)[0]
+		numberB = int(b)
+		numberE = int(e)
+		#print numberB
+		#print numberE
+		sum = count(numberE, numberB)
+		s.send(str(sum) + '\n')
+
